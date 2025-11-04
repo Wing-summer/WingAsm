95e28056cf5b76e4744bef72336d72f67982b83a
wingsummer <1326224942@qq.com>
2025-11-04 20:08:42 +0800

    fix: 修复编译问题和增加英文翻译；

---
+++ WingHexAsm_en_US.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AsmWindow</name>
+    <message>
+        <location filename="src/asmwindow.ui" line="34"/>
+        <source>Arch</source>
+        <translation>Architecture</translation>
+    </message>
+    <message>
+        <location filename="src/asmwindow.ui" line="51"/>
+        <source>Style</source>
+        <translation>Style</translation>
+    </message>
+    <message>
+        <location filename="src/asmwindow.ui" line="79"/>
+        <source>Process</source>
+        <translation>Process</translation>
+    </message>
+</context>
+<context>
+    <name>WingHexAsm</name>
+    <message>
+        <location filename="src/winghexasm.cpp" line="65"/>
+        <source>WingHexAsm</source>
+        <translation>WingHexAsm</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="72"/>
+        <source>Assembler/Disassembler plugin for WingHexExplorer2</source>
+        <translation>Assembler/Disassembler plugin for WingHexExplorer2</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="133"/>
+        <source>WingAsm</source>
+        <translation>WingAsm</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="142"/>
+        <source>WingDisAsm</source>
+        <translation>WingDisAsm</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="241"/>
+        <source>NoInput</source>
+        <translation>Operation cannot proceed without input</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="246"/>
+        <location filename="src/winghexasm.cpp" line="290"/>
+        <source>NoCurrentFileEditing</source>
+        <translation>No document is currently available for editing</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="262"/>
+        <location filename="src/winghexasm.cpp" line="269"/>
+        <location filename="src/winghexasm.cpp" line="274"/>
+        <source>AsmWriteFailed</source>
+        <translation>Assembly write failed</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="281"/>
+        <source>AsmErrorSeeLog</source>
+        <translation>Assembly failed. Please check the log</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="296"/>
+        <source>OnlyOneSelSupport</source>
+        <translation>Supports single selection only</translation>
+    </message>
+    <message>
+        <location filename="src/winghexasm.cpp" line="312"/>
+        <source>DisAsmErrorSeeLog</source>
+        <translation>Disassembly failed. Please check the log</translation>
+    </message>
+</context>
+</TS>
